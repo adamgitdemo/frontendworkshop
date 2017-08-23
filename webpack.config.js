4e0291320d49deecb3db9995294f9d8c36984cbe
Adam Arczynski <adam.arczynski@ocado.com>
2017-08-23 20:14:32 +0200

    use sass

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
-                    use: ["css-loader?minimize=true"]
+                    use: ["css-loader?minimize=true", "sass-loader"]
                 })
             }
         ]
